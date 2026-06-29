a00496462d6e9a35351068af38d07dd91681f85e
NonaSuomy <nonasuomy@live.com>
2026-06-28 20:05:41 -0400

    Initial commit for Starlight docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,84 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  site: "https://BezerkRevived.github.io",
+  base: "/bezerk-docs",
+  integrations: [
+    starlight({
+      title: "Bezerk Documentation",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/BezerkRevived/bezerk-docs",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/MVEsDC2UXy",
+        },
+      ],
+      sidebar: [
+        {
+          label: "The Netshow",
+          collapsed: true,
+          items: [
+            { label: "Overview", slug: "games/netshow" },
+            { label: "Game Bots", slug: "games/netshow/game-bots" },
+            {
+              label: "Extracting Content",
+              slug: "games/netshow/extracting-content",
+            },
+            { label: "How-to's", slug: "games/netshow/how-tos" },
+            { label: "FAQ", slug: "games/netshow/faq" },
+          ],
+        },
+        {
+          label: "Acrophobia",
+          collapsed: true,
+          items: [
+            { label: "Overview", slug: "games/acrophobia" },
+            { label: "Game Bots", slug: "games/acrophobia/game-bots" },
+            {
+              label: "Extracting Content",
+              slug: "games/acrophobia/extracting-content",
+            },
+            { label: "How-to's", slug: "games/acrophobia/how-tos" },
+            { label: "FAQ", slug: "games/acrophobia/faq" },
+          ],
+        },
+        {
+          label: "Cosmic Consensus",
+          collapsed: true,
+          items: [
+            { label: "Overview", slug: "games/cosmic-consensus" },
+            { label: "Game Bots", slug: "games/cosmic-consensus/game-bots" },
+            {
+              label: "Extracting Content",
+              slug: "games/cosmic-consensus/extracting-content",
+            },
+            { label: "How-to's", slug: "games/cosmic-consensus/how-tos" },
+            { label: "FAQ", slug: "games/cosmic-consensus/faq" },
+          ],
+        },
+        {
+          label: "Get The Picture",
+          collapsed: true,
+          items: [
+            { label: "Overview", slug: "games/get-the-picture" },
+            { label: "Game Bots", slug: "games/get-the-picture/game-bots" },
+            {
+              label: "Extracting Content",
+              slug: "games/get-the-picture/extracting-content",
+            },
+            { label: "How-to's", slug: "games/get-the-picture/how-tos" },
+            { label: "FAQ", slug: "games/get-the-picture/faq" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
